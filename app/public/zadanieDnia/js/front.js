80b5150a59a73b32718cb87bba7932bc4714a77d
Jerzy Drobot <jerzy.drobot@mggp.com.pl>
2018-02-09 16:01:30 +0100

    Server instance and add task method

--- app/public/zadanieDnia/js/front.js
+++ app/public/zadanieDnia/js/front.js
@@ -1,1 +1,24 @@
 // Twój kod
+$(function () {
+
+
+
+    const addTask = function () {
+        //Send task to server
+        fetch('/add', {
+            method: 'POST',
+            body: JSON.stringify({
+                id: 1,
+                name: '',
+                done: false
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            }
+            //Action after server response
+        }).than(res => res.json())
+            .than(res => {
+                console.log(res);
+            })
+    }
+});
--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -1,1 +1,37 @@
 //Twój kod
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('./public/zadanieDnia'));
+
+app.post('/add', (req, res => {
+    fs.readFile('./data/data.json', (err, data) => {
+        if (!err) {
+            const taskList = JSON.parse(data);
+            const taskAdded = { id: req.body.id, name: req.body.name, done: req.body.done }
+            taskList.push(taskAdded)
+            const listToSave = JSON.stringify(taskList);
+            fs.writeFile('./data/data.json', listToSave, (err => {
+                if (!err) {
+                    res.send("Task list saved")
+                }
+                else {
+                    console.log('Error save database file!');
+                    res.send('Error save database file!');
+                }
+            }))
+        }
+        else {
+            console.log('Error read database file!');
+            res.send('Error read database file!');
+        }
+    })
+}))
+
+app.listen(3000, () => {
+    console.log('Serwer działa na porcie 3000');
+})
